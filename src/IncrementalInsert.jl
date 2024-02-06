74de95c78199fb3e3ef51ef930988bc239640dcb
Kaze Wong <kazewong.physics@gmail.com>
2024-02-06 14:54:49 -0500

    Refactor Delaunay tree initialization and improve in_sphere function

--- src/IncrementalInsert.jl
+++ src/IncrementalInsert.jl
@@ -11,13 +11,21 @@ function initialize_tree(points::Vector{Vertex})::DelaunayTree
     third_vertex = Vertex(3, center + [radius / sqrt(2) * cos(2 * pi / 3), radius / sqrt(2) * sin(2 * pi / 3), -radius / sqrt(2)])
     fourth_vertex = Vertex(4, center + [radius / sqrt(2) * cos(4 * pi / 3), radius / sqrt(2) * sin(4 * pi / 3), -radius / sqrt(2)])
     node = DelaunayTreeNode(1, false, [first_vertex, second_vertex, third_vertex, fourth_vertex])
-    return DelaunayTree([node], Dict(1 => []), Dict(1 => []), Dict(1 => []))
+    unbounded_node = Vector{DelaunayTreeNode}()
+    push!(unbounded_node, DelaunayTreeNode(2, false, [first_vertex, first_vertex, second_vertex, third_vertex]))
+    push!(unbounded_node, DelaunayTreeNode(3, false, [first_vertex, first_vertex, second_vertex, fourth_vertex]))
+    push!(unbounded_node, DelaunayTreeNode(4, false, [first_vertex, first_vertex, third_vertex, fourth_vertex]))
+    push!(unbounded_node, DelaunayTreeNode(5, false, [second_vertex, second_vertex, third_vertex, fourth_vertex]))
+    children_relation = Dict(1 => [], 2 => [], 3 => [], 4 => [], 5 => [])
+    step_children_relation = Dict(1 => [], 2 => [], 3 => [], 4 => [], 5 => [])
+    neighbors_relation = Dict(1 => [2, 3, 4, 5], 2 => [1], 3 => [1], 4 => [1], 5 => [1])
+    return DelaunayTree([node, unbounded_node...], children_relation, step_children_relation, neighbors_relation)
 end
 
 function in_sphere(node::DelaunayTreeNode, point::Vertex)::Bool
     position = reduce(hcat, map(x -> x.position, node.vertices)) .- point.position
     position = vcat(position, mapslices(norm, position, dims=1))
-    return det(position) > 0
+    return abs(det(position)) > 1e-15
 end
 
 function locate(visited_ids::Vector{Int}, output::Vector{Int}, vertex::Vertex, current_node::DelaunayTreeNode, tree::DelaunayTree)::Vector{Int}
@@ -46,5 +54,5 @@ end
 # end
 
 test_points = initialize_vertex(100)
-root_node = initialize_tree(test_points)
-println(locate(Vector{Int}(), Vector{Int}(), test_points[1], root_node.nodes[1], root_node))
+delaunay_tree = initialize_tree(test_points)
+println(locate(Vector{Int}(), Vector{Int}(), test_points[1], delaunay_tree.nodes[1], delaunay_tree))
