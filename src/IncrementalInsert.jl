09810c89572c7302e4e651da40d787f1cc77d2e9
Kaze Wong <kazewong.physics@gmail.com>
2024-02-06 22:36:02 -0500

    Add PlotlyJS and Plots packages

--- src/IncrementalInsert.jl
+++ src/IncrementalInsert.jl
@@ -1,16 +1,19 @@
 using Revise
 using BoundingSphere
 using LinearAlgebra
+using Plots
+plotlyjs()
 include("Primitives.jl")
 
 function initialize_tree(points::Vector{Vertex})::DelaunayTree
     positions = map(x -> x.position, points)
     center, radius = boundingsphere(positions)
+    radius = radius*2
     first_vertex = Vertex(-8, center + [0, 0, radius])
-    second_vertex = Vertex(-7, center + [radius / sqrt(2) * cos(0), radius / sqrt(2) * sin(0), -radius / sqrt(2)])
-    third_vertex = Vertex(-6, center + [radius / sqrt(2) * cos(2 * pi / 3), radius / sqrt(2) * sin(2 * pi / 3), -radius / sqrt(2)])
-    fourth_vertex = Vertex(-5, center + [radius / sqrt(2) * cos(4 * pi / 3), radius / sqrt(2) * sin(4 * pi / 3), -radius / sqrt(2)])
-    ghost_vertex = [Vertex(-4, center + [0, 0, radius]), Vertex(-3, center + [0, 0, radius]), Vertex(-2, center + [0, 0, radius]), Vertex(-1, center + [radius / sqrt(2) * cos(0), radius / sqrt(2) * sin(0), -radius / sqrt(2)])]
+    second_vertex = Vertex(-7, center + [radius * cos(0), radius * sin(0), -radius ])
+    third_vertex = Vertex(-6, center + [radius * cos(2 * pi / 3), radius * sin(2 * pi / 3), -radius])
+    fourth_vertex = Vertex(-5, center + [radius * cos(4 * pi / 3), radius * sin(4 * pi / 3), -radius])
+    ghost_vertex = [Vertex(-4, center + [0, 0, radius]), Vertex(-3, center + [0, 0, radius]), Vertex(-2, center + [0, 0, radius]), Vertex(-1, center + [radius  * cos(0), radius * sin(0), -radius])]
     verticies = [first_vertex, second_vertex, third_vertex, fourth_vertex, ghost_vertex...]
     verticies = Dict(map(x -> x.id => x, verticies))
     node = DelaunayTreeNode(1, false, [-8, -7, -6, -5])
@@ -29,7 +32,7 @@ end
 function in_sphere(node_id::Int, point::Vertex, tree::DelaunayTree)::Bool
     position = reduce(hcat, map(x -> x.position, map(x->tree.vertices[x], tree.simplices[node_id].vertices))) .- point.position
     position = vcat(position, mapslices(norm, position, dims=1))
-    return abs(det(position)) > 1e-15
+    return det(position) > 1e-15
 end
 
 function locate(visited_ids::Vector{Int}, output::Vector{Int}, vertex::Vertex, current_node_id::Int, tree::DelaunayTree)::Vector{Int}
@@ -39,12 +42,10 @@ function locate(visited_ids::Vector{Int}, output::Vector{Int}, vertex::Vertex, c
         push!(output, current_node_id)
         # end
         childrens = tree.children_relation[current_node_id]
-        check_children = Vector{Int}()
         for child_id in childrens
             locate(visited_ids, output, vertex, child_id, tree)
         end
         step_childrens = collect(values(tree.step_children_relation[current_node_id]))
-        check_step_children = Vector{Int}()
         for step_children_id in step_childrens
             locate(visited_ids, output, vertex, step_children_id, tree)
         end
@@ -65,6 +66,7 @@ end
 
 function insert_point(tree::DelaunayTree, point::Vertex)
     killed_nodes = locate(Vector{Int}(), Vector{Int}(), point, 1, tree)
+    println("killed_nodes: ", killed_nodes)
     new_node_id = Vector{Int}()
     for node_id in killed_nodes
         if !tree.simplices[node_id].dead
@@ -75,6 +77,8 @@ function insert_point(tree::DelaunayTree, point::Vertex)
                     if length(facet) == 3
                         # Creating new node
                         new_id = length(tree.simplices) + 1
+                        println("new_id: ", new_id)
+                        println("facet: ", facet)
                         new_node = DelaunayTreeNode(new_id, false, [point.id, facet...])
                         tree.simplices[new_id] = new_node
                         push!(new_node_id, new_node.id)
@@ -92,7 +96,7 @@ function insert_point(tree::DelaunayTree, point::Vertex)
 
                         # Updating neighbor relationship for the neighbor of the killed node
                         killed_node_id = findfirst(x->x==node_id, tree.neighbors_relation[neighbor_id])
-                        if killed_node_id != nothing
+                        if killed_node_id !== nothing
                             tree.neighbors_relation[neighbor_id][findfirst(x->x==node_id, tree.neighbors_relation[neighbor_id])] = new_node.id
                         end
                     end
@@ -106,7 +110,6 @@ function insert_point(tree::DelaunayTree, point::Vertex)
             new_id2 = new_node_id[j]
             facet = common_facet(tree.simplices[new_id1], tree.simplices[new_id2])
             if length(facet) == 3
-                println("facet: ", facet)
                 push!(tree.neighbors_relation[new_id1], new_id2)
                 push!(tree.neighbors_relation[new_id2], new_id1)
             end
@@ -116,8 +119,22 @@ function insert_point(tree::DelaunayTree, point::Vertex)
 end
 
 test_points = initialize_vertex(100)
-delaunay_tree = initialize_tree(test_points)
+tree = initialize_tree(test_points)
 # println(locate(Vector{Int}(), Vector{Int}(), test_points[1], 1, delaunay_tree))
-insert_point(delaunay_tree, test_points[1])
-insert_point(delaunay_tree, test_points[2])
-insert_point(delaunay_tree, test_points[3])
+insert_point(tree, test_points[1])
+insert_point(tree, test_points[2])
+
+new_tree = deepcopy(tree)
+
+
+x,y,z = [], [], []
+for simplex in values(tree.simplices)
+    for vertex_id in simplex.vertices
+        push!(x, tree.vertices[vertex_id].position[1])
+        push!(y, tree.vertices[vertex_id].position[2])
+        push!(z, tree.vertices[vertex_id].position[3])
+    end
+end
+
+path3d(x, y, z, label="Points", size=(800, 800))
+scatter!([test_points[2].position[1]], [test_points[2].position[2]], [test_points[2].position[3]], label="New Point", color="red")
