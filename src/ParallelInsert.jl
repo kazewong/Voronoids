2dddc225564c97c3d1c8a4e596f368e1de1e4774
Kaze Wong <kazewong.physics@gmail.com>
2024-02-15 23:40:59 -0500

    Refactor parallel_locate and identify_nonconflict_points functions

--- src/ParallelInsert.jl
+++ src/ParallelInsert.jl
@@ -1,21 +1,21 @@
-function batch_locate(vertices::AbstractArray, tree::DelaunayTree)::Vector{Vector{Int}}
-    output = Vector{Vector{Int}}(undef, length(vertices))
+function batch_locate!(output::AbstractArray,vertices::AbstractArray, tree::DelaunayTree)
     for i in 1:length(vertices)
         output[i] = locate(Vector{Int}(), vertices[i], tree)
+    end
+end
+
+function parallel_locate!(vertices::Vector{Vector{Float64}}, tree::DelaunayTree)::Vector{Vector{Int}}
+    output = Vector{Vector{Int}}(undef, length(vertices))
+    batch_size = length(vertices) ÷ Threads.nthreads()
+    output_batch = Iterators.partition(output, batch_size)
+    vertices = Iterators.partition(vertices, batch_size)
+    map(output_batch,vertices) do local_output, vertice
+        Threads.@spawn batch_locate(local_output, vertice, tree)
     end
     return output
 end
 
-function parallel_locate(vertices::Vector{Vector{Float64}}, tree::DelaunayTree)::Vector{Vector{Int}}
-    batch_size = length(vertices) ÷ Threads.nthreads()
-    vertices = Iterators.partition(vertices, batch_size)
-    task = map(vertices) do vertice
-        Threads.@spawn batch_locate(vertice, tree)
-    end
-    return reduce(vcat, fetch.(task))
-end
-
-function identify_nonconflict_points(vertices::Vector{Vector{Float64}}, tree::DelaunayTree)::Vector{Int}
+function identify_nonconflict_points(vertices::Vector{Vector{Float64}}, tree::DelaunayTree)::Tuple{Vector{Vector{Int}},Vector{Int}}
     site_list = parallel_locate(vertices, tree)
     length_site_list = length(site_list)
     output = Vector{Bool}(undef, length_site_list)
@@ -23,7 +23,16 @@ function identify_nonconflict_points(vertices::Vector{Vector{Float64}}, tree::De
         # output[i] = all(isempty.(map(x->intersect(reduce(vcat,tree.neighbors_relation[site_list[i]]), reduce(vcat, tree.neighbors_relation[site_list[x]])), i+1:length_site_list)))
         output[i] = all(isempty.(map(x->intersect(site_list[i], site_list[x]), i+1:length_site_list)))
     end
-    return output
+    return site_list, output
+end
+
+function batch_insert_point(vertices::Vector{Vector{Float64}}, tree::DelaunayTree)
+    nonconflict_points = identify_nonconflict_points(vertices, tree)
+    for i in 1:length(vertices)
+        if nonconflict_points[i]
+            insert_point(tree, vertices[i])
+        end
+    end
 end
 
 export parallel_locate, batch_locate, identify_nonconflict_points
