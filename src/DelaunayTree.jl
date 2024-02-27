a2a7160fcbb086526fecccb53477d58609eab8ff
Kaze Wong <kazewong.physics@gmail.com>
2024-02-27 15:51:41 -0500

    Refactor data structures to use dictionaries instead of vectors

--- src/DelaunayTree.jl
+++ src/DelaunayTree.jl
@@ -6,10 +6,10 @@ mutable struct DelaunayTree
     kdtree::KDTree
     vertices_simplex::Vector{Vector{Int}}
 
-    simplices::Vector{Vector{Int}}
-    centers::Vector{Vector{Float64}}
-    radii::Vector{Float64}
-    neighbors_relation::Vector{Vector{Int}}
+    simplices::Dict{Int, Vector{Int}}
+    centers::Dict{Int, Vector{Float64}}
+    radii::Dict{Int, Float64}
+    neighbors_relation::Dict{Int, Vector{Int}}
 end
 
 struct TreeUpdate
@@ -19,8 +19,8 @@ struct TreeUpdate
     simplices_id::Vector{Int} # Id of the new simplices. Relatively indexed, i.e. max(simplices_id) = length(simplices)
     centers::Vector{Vector{Float64}} # Centers of the circumsphere of the new simplices
     radii::Vector{Float64} # Radii of the circumsphere of the new simplices
-    neighbors_id::Vector{Tuple{Int, Int}} # (Neighbor Id, killed site Id)
-    new_neighbors_id::Vector{Tuple{Int, Int}} # (New site Id1, New site Id2). Relatively indexed.
+    neighbors_id::Dict{Int, Int} # (Neighbor Id, killed site Id)
+    new_neighbors_id::Dict{Int, Int} # (New site Id1, New site Id2). Relatively indexed.
 end
 
 function insert_point!(tree::DelaunayTree, update::TreeUpdate)
@@ -67,15 +67,16 @@ function initialize_tree_3d(positions::Vector{Vector{Float64}})::DelaunayTree
     fourth_vertex = center + [radius * cos(4 * pi / 3), radius * sin(4 * pi / 3), -radius]
     ghost_vertex = [center + [0, 0, radius], center + [0, 0, radius], center + [0, 0, radius], center + [radius  * cos(0), radius * sin(0), -radius]]
     vertices = [first_vertex, second_vertex, third_vertex, fourth_vertex, ghost_vertex...]
-
-    simplicies = [[1, 2, 3, 4], [5, 1, 2, 3], [6, 1, 3, 4], [7, 1, 4, 2], [8, 2, 3, 4]]
-    vertices_simplex = [[1,2,3,4], [1, 2, 4, 5], [1, 2, 3, 5], [1, 3, 4, 5], [2], [3], [4], [5]]
-    centers = [center, [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    radii = [radius, 0, 0, 0, 0]
-
-    neighbors_relation = [[2, 3, 4, 5], [1], [1], [1], [1]]
     kd_tree = KDTree(reduce(hcat, vertices))
-    return DelaunayTree(vertices, kd_tree, simplicies, vertices_simplex, centers, radii, neighbors_relation)
+    vertices_simplex = [[1, 2, 3, 4], [1, 2, 4, 5], [1, 2, 3, 5], [1, 3, 4, 5], [2], [3], [4], [5]]
+
+    simplicies = Dict(1 => [1, 2, 3, 4], 2 => [5, 1, 2, 3], 3 => [6, 1, 3, 4], 4 => [7, 1, 4, 2], 5 => [8, 2, 3, 4])
+    centers = Dict(1 => center, 2 => [0, 0, 0], 3 => [0, 0, 0], 4 => [0, 0, 0], 5 => [0, 0, 0])
+    radii = Dict(1 => radius, 2 => 0, 3 => 0, 4 => 0, 5 => 0)
+    neighbors_relation = Dict(1 => [2, 3, 4, 5], 2 => [1], 3 => [1], 4 => [1], 5 => [1])
+    
+
+    return DelaunayTree(vertices, kd_tree, vertices_simplex, simplicies, centers, radii, neighbors_relation)
 end
 
 function initialize_tree_2d(positions::Vector{Vector{Float64}})::DelaunayTree
@@ -86,15 +87,15 @@ function initialize_tree_2d(positions::Vector{Vector{Float64}})::DelaunayTree
     third_vertex = center + [radius * cos(4 * pi / 3), radius * sin(4 * pi / 3)]
     ghost_vertex = [center + [radius * cos(0 * pi / 3), radius * sin(0 * pi / 3)], center + [radius * cos(2 * pi / 3), radius * sin(2 * pi / 3)], center + [radius * cos(4 * pi / 3), radius * sin(4 * pi / 3)]]
     vertices = [first_vertex, second_vertex, third_vertex, ghost_vertex...]
-
-    simplicies = [[1, 2, 3], [4, 1, 2], [6, 1, 3], [5, 2, 3]]
-    vertices_simplex = [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2], [3], [4]]
-    centers = [center, [0, 0], [0, 0], [0, 0]]
-    radii = [radius, 0, 0, 0]
-
-    neighbors_relation = [[2, 3, 4], [1], [1], [1]]
     kd_tree = KDTree(reduce(hcat, vertices))
-    return DelaunayTree(vertices, kd_tree, simplicies, vertices_simplex, centers, radii, neighbors_relation)
+    vertices_simplex = [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2], [3], [4]]
+
+    simplicies = Dict(1 => [1, 2, 3], 2 => [4, 1, 2], 3 => [6, 1, 3], 4 => [5, 2, 3])
+    centers = Dict(1 => center, 2 => [0, 0], 3 => [0, 0], 4 => [0, 0])
+    radii = Dict(1 => radius, 2 => 0, 3 => 0, 4 => 0)
+    neighbors_relation = Dict(1 => [2, 3, 4], 2 => [1], 3 => [1], 4 => [1])
+    
+    return DelaunayTree(vertices, kd_tree, vertices_simplex, simplicies, centers, radii, neighbors_relation)
 end
 
 export initialize_tree_2d, initialize_tree_3d, DelaunayTree
