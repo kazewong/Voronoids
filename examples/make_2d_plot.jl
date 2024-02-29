da55b4ef17e903ca85eaeaa57ae014d5b0c6ac9f
Kaze Wong <kazewong.physics@gmail.com>
2024-02-29 15:55:40 -0500

    Update number of test points and add 2D plot visualization

--- examples/make_2d_plot.jl
+++ examples/make_2d_plot.jl
@@ -5,7 +5,7 @@ using Random
 
 Random.seed!(1234)
 
-n = 10
+n = 5
 n_dims = 2
 
 test_points = [rand(n_dims) for i in 1:n]
@@ -13,7 +13,40 @@ test_points = [rand(n_dims) for i in 1:n]
 tree = initialize_tree_2d(test_points)
 
 for i in 1:n
-    println(i)
     add_vertex!(tree, test_points[i], n_dims=n_dims)
 end
 
+function plot_simplex_2d(simplex_Id::Int, tree::DelaunayTree)
+    x, y = [], []
+    vertices = tree.vertices[tree.simplices[simplex_Id]]
+    for vertex_id in [1,2,3,1]
+        push!(x, vertices[vertex_id][1])
+        push!(y, vertices[vertex_id][2])
+    end
+    return x, y
+end
+
+function plot_circle(center::Vector{Float64}, radius::Float64)
+    θ = LinRange(0, 2π, 100)
+    x = center[1] .+ radius * cos.(θ)
+    y = center[2] .+ radius * sin.(θ)
+    return x, y
+end
+
+scatter(map(x -> x[1], test_points), map(x -> x[2], test_points), c=distinguishable_colors(n)) 
+
+for ids in collect(keys(tree.simplices))
+    if all(tree.simplices[ids].>6)
+        x,y = plot_simplex_2d(ids, tree)
+        p = plot!(x, y, size=(800, 800))
+        x,y = plot_circle(tree.centers[ids], tree.radii[ids])
+        p = plot!(x, y, size=(800, 800))
+    end
+end
+
+display(p)
+# x,y = plot_simplex_2d(1, tree)
+# plot(x, y, label="Points", size=(800, 800))
+
+# p = scatter!(map(x -> x[1], test_points), map(x -> x[2], test_points), label="Points", c=distinguishable_colors(n)) 
+# check_delaunay(tree, n_dims=2)
