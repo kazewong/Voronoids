19a8dda8efddecaf7ce7857f66cb4ecffcb66a1d
Kaze Wong <kazewong.physics@gmail.com>
2024-03-12 17:44:44 -0400

    Delaunay is not working yet

--- tests/test_delaunay_tree.rs
+++ tests/test_delaunay_tree.rs
@@ -1,4 +1,6 @@
 use voronoids::delaunay_tree::{DelaunayTree, TreeUpdate};
+use rand::prelude::*;
+use rand::distributions::{Uniform, Distribution};
 
 #[test]
 fn test_delaunay_tree() {
@@ -10,8 +12,13 @@ fn test_delaunay_tree() {
     ];
     let mut delaunay_tree = DelaunayTree::new(vertices);
     assert_eq!(delaunay_tree.max_simplex_id, 4);
-    let new_point = [0.5, 0.5, 0.5];
-    let update = TreeUpdate::new(new_point, &delaunay_tree);
-    delaunay_tree.insert_point(update);
+    let mut rng = StdRng::seed_from_u64(0);
+    let dist = Uniform::from(0.0..1.0);
+
+    for _ in 0..100 {
+        let new_point = [dist.sample(&mut rng), dist.sample(&mut rng), dist.sample(&mut rng)];
+        let update = TreeUpdate::new(new_point, &delaunay_tree);
+        delaunay_tree.insert_point(update);
+    }
     assert!(delaunay_tree.check_delaunay());
 }
