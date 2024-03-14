45546f4392701390d6b5a3f5ad55ca7171fcede0
kazewong <kazewong.physics@gmail.com>
2024-03-14 10:49:19 -0400

    Adding bounding sphere method

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,7 @@
+use std::process::Output;
+
 fn circumsphere_2d(vertices: [[f64; 2]; 3]) -> ([f64; 2], f64) {
-    
-    let [x1,y1] = vertices[0];
+    let [x1, y1] = vertices[0];
     let [x2, y2] = vertices[1];
     let [x3, y3] = vertices[2];
 
@@ -16,9 +17,9 @@ fn circumsphere_2d(vertices: [[f64; 2]; 3]) -> ([f64; 2], f64) {
     let X = (mD * D[0] - mE * E[0] + E[1] - D[1]) / (mD - mE);
     let Y = mD * (X - D[0]) + D[1];
 
-    let R = ((X - x1)*(X - x1) + (Y - y1)*(Y - y1)).sqrt();
+    let R = ((X - x1) * (X - x1) + (Y - y1) * (Y - y1)).sqrt();
 
-    return ([X, Y], R)
+    return ([X, Y], R);
 }
 
 fn circumsphere_3d(vertices: [[f64; 3]; 4]) -> ([f64; 3], f64) {
@@ -73,15 +74,15 @@ fn circumsphere_3d(vertices: [[f64; 3]; 4]) -> ([f64; 3], f64) {
     (center, radius.sqrt())
 }
 
-pub fn circumsphere<const N:usize, const M:usize>(vertices: [[f64; N]; M]) -> ([f64; N], f64) {
+pub fn circumsphere<const N: usize, const M: usize>(vertices: [[f64; N]; M]) -> ([f64; N], f64) {
     let mut center = [0.0; N];
     let mut radius = 0.0;
-    if N == 2{
+    if N == 2 {
         let vertices_2d = [
             [vertices[0][0], vertices[0][1]],
             [vertices[1][0], vertices[1][1]],
             [vertices[2][0], vertices[2][1]],
-            ];
+        ];
         let (center_2d, radius_2d) = circumsphere_2d(vertices_2d);
         for i in 0..N {
             center[i] = center_2d[i];
@@ -103,10 +104,40 @@ pub fn circumsphere<const N:usize, const M:usize>(vertices: [[f64; N]; M]) -> ([
     (center, radius)
 }
 
-pub fn in_sphere<const N:usize>(vertex: [f64; N], center: [f64; N], radius: f64) -> bool {
+pub fn in_sphere<const N: usize>(vertex: [f64; N], center: [f64; N], radius: f64) -> bool {
     let mut distance: f64 = 0.0;
     for i in 0..N {
         distance += (center[i] - vertex[i]) * (center[i] - vertex[i]);
     }
     distance < radius * radius
+}
+
+pub fn bounding_sphere<const N: usize>(vertices: Vec<[f64; N]>) -> ([f64; N], f64) {
+    // Compute a bounding sphere for a given set of points.
+    // It is not a minimal bounding sphere, but we don't need it to be minimum anyway
+    let mut inside: Vec<bool> = vec![false; vertices.len()];
+    let mut lower_corner: [f64; N] = [0.0; N];
+    let mut upper_corner: [f64; N] = [0.0; N];
+    let mut center: [f64; N] = [0.0; N];
+    let mut radius: f64 = 0.0;
+    for i in 0..N {
+        lower_corner[i] = vertices.iter().map(|x| x[i]).fold(f64::INFINITY, f64::min);
+        upper_corner[i] = vertices.iter().map(|x| x[i]).fold(f64::NEG_INFINITY, f64::max);
+        center[i] = (upper_corner[i] + lower_corner[i])/2.0;
+    }
+    let upper_distance = upper_corner.iter().zip(center.iter()).map(|(x, y)| (x - y)*(x - y)).sum::<f64>().sqrt();
+    let lower_distance = lower_corner.iter().zip(center.iter()).map(|(x, y)| (x - y)*(x - y)).sum::<f64>().sqrt();
+    radius = if upper_distance > lower_distance {upper_distance} else {lower_distance};
+    // Check if all points are inside the sphere.
+    for (id, point) in vertices.iter().enumerate() {
+        if inside[id] == false{
+            if in_sphere(*point, center, radius) {
+                inside[id] = true;
+            }
+            else {
+                radius = radius *1.5;
+            }
+        }
+    }
+    (center, radius)
 }
--- tests/test_geometry.rs
+++ tests/test_geometry.rs
@@ -1,5 +1,7 @@
-use voronoids::geometry::circumsphere;
-
+use parry3d_f64::query::point;
+use rand::distributions::{Distribution, Uniform};
+use rand::prelude::*;
+use voronoids::geometry::{bounding_sphere, circumsphere};
 #[test]
 fn test_circumsphere() {
     let vertices = [
@@ -11,4 +13,22 @@ fn test_circumsphere() {
     let (center, radius) = circumsphere(vertices);
     assert_eq!(center, [0.5, 0.5, 0.5]);
     assert_eq!(radius, 0.8660254037844386);
+}
+
+#[test]
+fn test_boundsphere() {
+    const n_test: usize = 1000;
+    let mut rng = StdRng::seed_from_u64(0);
+    let dist = Uniform::from(-1.0..1.0);
+    let mut point_test: [[f64; 3]; n_test] = [[0.0; 3]; n_test];
+    for i in 0..n_test {
+        point_test[i] = [
+            dist.sample(&mut rng),
+            dist.sample(&mut rng),
+            dist.sample(&mut rng),
+        ];
+    }
+    let (center, radius) = bounding_sphere(point_test.to_vec());
+    println!("Center: {:?}", center);
+    println!("Radius: {:?}", radius);
 }
