7068077c3776f4d30ea8563d001b9edddff69647
Kaze Wong <kazewong.physics@gmail.com>
2024-03-17 15:00:22 -0400

    Refactor make_queue function in scheduler.rs

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashMap, hash::Hash};
 use rayon::prelude::*;
+use std::{collections::HashMap, hash::Hash};
 
 use crate::delaunay_tree::DelaunayTree;
 
@@ -7,18 +7,25 @@ pub fn make_queue<const N: usize, const M: usize>(
     vertices: Vec<[f64; N]>,
     tree: DelaunayTree<N, M>,
 ) -> Vec<(usize, [f64; N], Vec<usize>)> {
-    vertices.into_par_iter().enumerate().map(|(id, vertex)| {
-        let killed_site = tree.locate(vertex);
-        let mut neighbors:Vec<usize> = killed_site.into_iter().map(|site| {
-            tree.neighbors.get(&site).unwrap().clone()
-        }).flatten().collect();
-        neighbors.sort();
-        neighbors.dedup();
-        (id, vertex, neighbors)
-    }).collect()
+    vertices
+        .into_par_iter()
+        .enumerate()
+        .map(|(id, vertex)| {
+            let killed_site = tree.locate(vertex);
+            let mut neighbors: Vec<usize> = killed_site
+                .into_iter()
+                .map(|site| tree.neighbors.get(&site).unwrap().clone())
+                .flatten()
+                .collect();
+            neighbors.sort();
+            neighbors.dedup();
+            (id, vertex, neighbors)
+        })
+        .collect()
 }
 
-pub fn find_placement(queue: Vec<(usize, [f64; 3], Vec<usize>)>) {//-> Vec<usize>{
+pub fn find_placement(queue: Vec<(usize, [f64; 3], Vec<usize>)>) {
+    //-> Vec<usize>{
     let mut occupancy: HashMap<usize, Vec<usize>> = HashMap::new();
     queue.into_iter().for_each(|(id, vertex, neighbors)| {
         for neighbor in neighbors {
@@ -30,5 +37,4 @@ pub fn find_placement(queue: Vec<(usize, [f64; 3], Vec<usize>)>) {//-> Vec<usize
             }
         }
     });
-    println!("{:?}", occupancy);
 }
