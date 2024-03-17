d7b96ebdbc9b4231af96e7eb854e103b1bedcdba
Kaze Wong <kazewong.physics@gmail.com>
2024-03-17 15:44:23 -0400

    Refactor make_queue function and add find_placement function

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,3 +1,4 @@
+use nalgebra::zero;
 use rayon::prelude::*;
 use std::{collections::HashMap, hash::Hash};
 
@@ -17,6 +18,11 @@ pub fn make_queue<const N: usize, const M: usize>(
                 .map(|site| tree.neighbors.get(&site).unwrap().clone())
                 .flatten()
                 .collect();
+            neighbors = neighbors
+                .into_iter()
+                .map(|site| tree.neighbors.get(&site).unwrap().clone())
+                .flatten()
+                .collect();
             neighbors.sort();
             neighbors.dedup();
             (id, vertex, neighbors)
@@ -24,18 +30,39 @@ pub fn make_queue<const N: usize, const M: usize>(
         .collect()
 }
 
-pub fn find_placement(queue: Vec<(usize, [f64; 3], Vec<usize>)>) {
+pub fn find_placement(queue: Vec<(usize, [f64; 3], Vec<usize>)>) -> Vec<usize> {
     //-> Vec<usize>{
     let mut occupancy: HashMap<usize, Vec<usize>> = HashMap::new();
-    queue.into_iter().for_each(|(id, vertex, neighbors)| {
-        for neighbor in neighbors {
-            if occupancy.contains_key(&neighbor) {
-                let mut sites = occupancy.get_mut(&neighbor).unwrap();
-                sites.push(id);
-            } else {
-                occupancy.insert(neighbor, vec![id]);
+    queue
+        .clone()
+        .into_iter()
+        .for_each(|(id, vertex, neighbors)| {
+            for neighbor in neighbors {
+                if occupancy.contains_key(&neighbor) {
+                    let mut sites = occupancy.get_mut(&neighbor).unwrap();
+                    sites.push(id);
+                } else {
+                    occupancy.insert(neighbor, vec![id]);
+                }
             }
+        });
+    let length = queue.len();
+    let mut placement: Vec<usize> = Vec::with_capacity(length);
+    for i in 0..length {
+        let mut max_distance = 0;
+        for neighbor in queue[i].2.clone() {
+            let mut current_distance = 0;
+            for site in occupancy.get(&neighbor).unwrap() {
+                if *site == i {
+                    current_distance += 1;
+                    break;
+                }else{
+                    current_distance = std::cmp::max(current_distance, placement[*site]);
+                }
+            }
+            max_distance = std::cmp::max(max_distance, current_distance);
         }
-    });
-    
+        placement.push(max_distance);
+    }
+    placement
 }
--- tests/test_scheduler.rs
+++ tests/test_scheduler.rs
@@ -5,23 +5,32 @@ use voronoids::scheduler::find_placement;
 
 #[test]
 fn test_queuing() {
+    const N_POINTS: usize = 1000;
     let mut vertices = vec![];
+    let mut vertices2 = vec![];
     let mut rng = StdRng::seed_from_u64(0);
     let dist = Uniform::from(0.0..1.0);
-    for _ in 0..100 {
+    for _ in 0..N_POINTS {
         let point = [
             dist.sample(&mut rng),
             dist.sample(&mut rng),
             dist.sample(&mut rng),
         ];
         vertices.push(point);
+        let point2 = [
+            dist.sample(&mut rng),
+            dist.sample(&mut rng),
+            dist.sample(&mut rng),
+        ];
+        vertices2.push(point2);
     }
     let mut delaunay_tree = DelaunayTree::<3, 4>::new(vertices.clone());
-    for i in 0..100 {
+    for i in 0..N_POINTS {
         let update = TreeUpdate::new(vertices[i], &delaunay_tree);
         delaunay_tree.insert_point(update);
     }
 
-    let queue = voronoids::scheduler::make_queue(vertices, delaunay_tree);
-    find_placement(queue);
+    let queue = voronoids::scheduler::make_queue(vertices2, delaunay_tree);
+    let placement = find_placement(queue);
+    println!("{:?}", placement);
 }
