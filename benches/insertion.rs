14c9dec6243d7f73d73acdf9a5bb452b9691baa9
Kaze Wong <kazewong.physics@gmail.com>
2024-03-23 17:46:24 -0400

    Update benchmark_locate function in insertion.rs

--- benches/insertion.rs
+++ benches/insertion.rs
@@ -1,11 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use rand::{distributions::{Distribution, Uniform}, rngs::StdRng, SeedableRng};
-use voronoids::delaunay_tree::{self, DelaunayTree, TreeUpdate};
+use voronoids::delaunay_tree::{DelaunayTree, TreeUpdate};
 
 fn benchmark_locate(c: &mut Criterion){
-
-    const N_POINTS: usize = 1000;
+    const N_POINTS: usize = 10000;
     let mut vertices = vec![];
     let mut rng = StdRng::seed_from_u64(0);
     let dist = Uniform::from(0.0..1.0);
@@ -19,7 +18,8 @@ fn benchmark_locate(c: &mut Criterion){
     }
     let mut delaunay_tree = DelaunayTree::<3, 4>::new(vertices.clone());
     let new_vertex = [0.5, 0.5, 0.5];
-    c.bench_function("locate 10", |b| b.iter(|| delaunay_tree.locate(black_box(new_vertex))));
+    c.bench_function("locate 10000", |b| b.iter(|| delaunay_tree.locate(new_vertex)));
+    c.bench_function("update 10000", |b| b.iter(|| TreeUpdate::new(10001, new_vertex, &delaunay_tree)));
 }
 
 criterion_group!(benches, benchmark_locate);
