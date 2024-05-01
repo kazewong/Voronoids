d28ab50bf7c92137502766cafeee2a55c010ece8
kazewong <kazewong.physics@gmail.com>
2024-05-01 11:15:31 -0400

    Making getter skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,21 @@ pub mod delaunay_tree;
 pub mod geometry;
 pub mod scheduler;
 
-use delaunay_tree::DelaunayTree;
+use dashmap::DashMap;
+use delaunay_tree::{DelaunayTree, Vertex};
 use pyo3::prelude::*;
 
 #[pyclass]
 struct PyDelauanyTree {
     tree: DelaunayTree<3, 4>,
+}
+
+#[pymethods]
+impl PyDelauanyTree{
+    #[getter]
+    fn max_simplex_id(&self) -> usize {
+        self.tree.max_simplex_id
+    }
 }
 
 #[pyfunction]
@@ -21,6 +30,7 @@ fn delaunay(points: Vec<[f64; 3]>)-> PyDelauanyTree {
 
 #[pymodule]
 fn voronoids(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyDelauanyTree>()?;
     m.add_function(wrap_pyfunction!(delaunay, m)?)?;
     Ok(())
 }
