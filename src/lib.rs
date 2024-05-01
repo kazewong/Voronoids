f2f624cb042af3c5bfe7657f74df0fdd0831a4b9
kazewong <kazewong.physics@gmail.com>
2024-05-01 10:51:45 -0400

    Add pyO3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 #![crate_name = "voronoids"]
 
 pub mod delaunay_tree;
-pub mod scheduler;
 pub mod geometry;
+pub mod scheduler;
+
+use delaunay_tree::DelaunayTree;
+use pyo3::prelude::*;
+
+#[pyclass]
+struct PyDelauanyTree {
+    tree: DelaunayTree<3, 4>,
+}
+
+#[pyfunction]
+fn delaunay(points: Vec<[f64; 3]>)-> PyDelauanyTree {
+    PyDelauanyTree {
+        tree: DelaunayTree::<3, 4>::new(points),
+    }
+}
